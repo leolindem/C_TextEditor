224e86f968e2ec378a754593a43cf77f1571cc2f
Leonardo Carneiro Lindemberg <leolind@umich.edu>
2024-12-22 17:40:01 -0500

    page up and page down implemented

--- kilo.c
+++ kilo.c
@@ -12,7 +12,8 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
 
-enum editorKeys {
+enum editorKeys
+{
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -94,24 +95,42 @@ int editorReadKey(void)
 
         if (seq[0] == '[')
         {
-            switch (seq[1])
+            if (seq[1] >= '0' && seq[1] <= '9')
             {
-            case 'A':
-                return ARROW_UP;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_LEFT;
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                }
             }
         }
-
         return '\x1b';
-    }else{
+    }
+    else
+    {
         return c;
     }
-
 }
 
 int getCursorPosition(int *rows, int *cols)
@@ -253,22 +272,26 @@ void editorMoveCursor(int key)
     switch (key)
     {
     case ARROW_LEFT:
-        if (E.cx != 0){
+        if (E.cx != 0)
+        {
             E.cx--;
         }
         break;
     case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1){
+        if (E.cx != E.screencols - 1)
+        {
             E.cx++;
         }
         break;
     case ARROW_UP:
-        if (E.cy != 0) {
+        if (E.cy != 0)
+        {
             E.cy--;
         }
         break;
     case ARROW_DOWN:
-        if (E.cy != E.screenrows - 1){
+        if (E.cy != E.screenrows - 1)
+        {
             E.cy++;
         }
         break;
@@ -286,6 +309,16 @@ void editorProcessKeypress(void)
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+        int times = E.screenrows;
+        while (times--)
+        {
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+    }
 
     case ARROW_UP:
     case ARROW_DOWN:
