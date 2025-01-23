fef64f3f30caeffddd17aa3bc174dbcec48934ff
Leonardo Carneiro Lindemberg <leolind@umich.edu>
2025-01-23 17:07:09 -0500

    saving to disk implemented

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
@@ -62,6 +63,10 @@ struct editorConfig
 };
 
 struct editorConfig E;
+
+/*** prototypes ***/
+
+void editorSetStatusMessage(const char *fmt, ...);
 
 /*** terminal ***/
 
@@ -338,6 +343,31 @@ void editorInsertChar(int c)
 
 /*** file i/o ***/
 
+char *editorRowsToString(int *buflen){
+    int totlen = 0;
+    int j;
+
+    // Add the length of each row + newline char
+    for (j = 0; j < E.numrows; j++){
+        totlen += E.row[j].size + 1;
+    }
+    *buflen = totlen;
+
+    // Allocates a buffer for all the lines
+    char *buf = malloc(totlen);
+    char *p = buf;
+
+    // Loop through all rows and memcpy them to the end of the buf
+    for (j = 0; j < E.numrows; j++){
+        memcpy(p, E.row[j].chars, E.row[j].size);
+        p += E.row[j].size;
+        *p = '\n';
+        p++;
+    }
+
+    return buf;
+}
+
 void editorOpen(char *filename)
 {
     free(E.filename);
@@ -361,6 +391,28 @@ void editorOpen(char *filename)
     }
     free(line);
     fclose(fp);
+}
+
+void editorSave(void) {
+    if (E.filename == NULL) return;
+
+    int len;
+    char *buf = editorRowsToString(&len);
+
+    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+    if (fd != -1){
+        if (ftruncate(fd, len) != -1) {
+            if (write(fd, buf, len) == len) {
+                close(fd);
+                free(buf);
+                editorSetStatusMessage("%d bytes written to disk", len);
+                return;
+            }
+        }
+        close(fd);
+    }
+    free(buf);
+    editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
 /*** append buffer ***/
@@ -603,6 +655,10 @@ void editorProcessKeypress(void)
         exit(0);
         break;
 
+    case CTRL_KEY('s'):
+        editorSave();
+        break;
+
     case HOME_KEY:
         E.cx = 0;
         break;
@@ -685,7 +741,7 @@ int main(int argc, char *argv[])
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 
     while (1)
     {
