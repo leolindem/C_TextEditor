2b63d0439e9de3b0488381ccfda2c016033ae617
Leonardo Carneiro Lindemberg <leolind@umich.edu>
2025-01-23 18:47:08 -0500

    handling backspacing at the end of a line

--- kilo.c
+++ kilo.c
@@ -308,6 +308,19 @@ void editorAppendRow(char *s, size_t len)
     E.dirty++;
 }
 
+void editorFreeRow(erow *row){
+    free(row->render);
+    free(row->chars);
+}
+
+void editorDelRow(int at){
+    if (at < 0 || at >= E.numrows) return;
+    editorFreeRow(&E.row[at]);
+    memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+    E.numrows--;
+    E.dirty++;
+}
+
 void editorRowInsertChar(erow *row, int at, int c)
 {
     // Bound checking for the insertion point
@@ -325,6 +338,15 @@ void editorRowInsertChar(erow *row, int at, int c)
 
     // Actually inserts the char
     row->chars[at] = c;
+    editorUpdateRow(row);
+    E.dirty++;
+}
+
+void editorRowAppendString(erow *row, char *s, size_t len){
+    row->chars = realloc(row->chars, row->size + len + 1);
+    memcpy(&row->chars[row->size], s, len);
+    row->size += len;
+    row->chars[row->size] = '\0';
     editorUpdateRow(row);
     E.dirty++;
 }
@@ -355,11 +377,17 @@ void editorInsertChar(int c)
 
 void editorDelChar(void) {
     if (E.cy == E.numrows) return;
+    if (E.cx == 0 && E.cy == 0) return;
 
     erow *row = &E.row[E.cy];
     if (E.cy > 0) {
         editorRowDelChar(row, E.cx - 1);
         E.cx--;
+    } else {
+        E.cx = E.row[E.cy - 1].size;
+        editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+        editorDelRow(E.cy);
+        E.cy--;
     }
 }
 
